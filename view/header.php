154000d0ab75e7255d9b9b2042456d498f1e3a47
jeniblok <116460893+jeniblok@users.noreply.github.com>
2025-07-01 20:59:12 -0500

    Update header.php
    updated as well

--- view/header.php
+++ view/header.php
@@ -5,11 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Manage your grocery list efficiently with this grocery management system.">
     <title>Grocery Dashboard</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="/grocery-mgt-app-main/css/style.css">
 </head>
 <body>
     <header>
         <h1>🥦 Grocery Management System</h1>
     </header>
-
-    <?php include 'nav.php'; ?>
